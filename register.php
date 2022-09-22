d6ece337a019386cd6ceef0022196038f9801cce
pratikjoshiauxirem <113657017+pratikjoshiauxirem@users.noreply.github.com>
2022-09-22 17:30:29 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,4 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <!--  <script src="fb.js" type="text/javascript" ></script> -->
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">FireBase</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Register</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
 <div class="container">
   <div class="row">
     <div class="col-6">
@@ -122,93 +176,8 @@ await setDoc(doc(db, "cities", "LA"), {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // const analytics = getAnalytics(app);
-  // function register(){
-  //   email=document.getElementById('email').value;
-  //     password=document.getElementById('password').value;
-  //       firstname=document.getElementById('firstname').value;
-  //         lastname=document.getElementById('lastname').value;
-
-  //         auth.createUserWithEmailAndPassword(email,password).then(function(){
-  //            var user=auth.currentUser;
-  //            var database_ref=database.ref();
-  //            var user_data={
-  //             email:email,
-  //             firstname:firstname,
-  //             lastname:lastname,
-  //             last_login: Date.now(),
-  //            }
-  //            database_ref.child('user/'+user.uid).set(user_data)
-  //            alert('user created'); 
-
-  //         }).catch(function(error){
-  //           var error_code=error.code;
-  //           var error_message=error.message;
-
-  //           alert(error_message);
-  //         })
-
-
-
-
-
-  // };
-
-
-  // function validate_email(email){
-  //   expression=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-  //   if(expression.test(email)==true){
-  //     return true
-  //   } else {
-  //     return false
-  //   }
-  // }
-  // function validate_password(password){
-  //   if(password<6){
-  //     return false
-  //   }
-  //   else{
-  //     return true
-  //   }
-  // }
-  //  function validate_field(){
-  //   if(field==null){
-  //     return false
-  //   }
-  //   if(field.length<=0){
-  //     return false
-  //   }
-  //   else {
-  //     return true
-  //   }
-  //  }
-
-
 </script>
+    </body>
+   </html>
+
  
